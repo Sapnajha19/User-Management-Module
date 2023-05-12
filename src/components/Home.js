23b10e78ed253a1e3016f5067d34b9defacd2f41
Sapna Jha <sapnajha1919@gmail.com>
2023-05-12 05:58:54 +0530

    Add Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
-
+import { useNavigate, Link, Routes, Route, useParams } from 'react-router-dom'
+import UserTable from './UserTable'
+import UserEdit from './UserEdit'
 export default function Home() {
+    const navigate = useNavigate()
     const [data, setData] = useState([])
     const [editIndex, setEditIndex] = useState(null)
 
@@ -9,15 +12,49 @@ export default function Home() {
             .then(res => res.json())
             .then(data => setData(data.list))
     }, [])
-    console.log(data)
-    const handleEditClick = (index) => {
-        setEditIndex(index)
-      }
-    // console.log(data.name)
+    // console.log(data)
+
+    const handleAddUser = (newUser) => {
+        setData([...data, newUser])
+    }
+
+    const handleUpdateUser = (updatedUser) => {
+        const newData = [...data];
+        console.log(newData)
+        const index = newData.findIndex(user => user.id === updatedUser.id);
+        newData[index] = updatedUser;
+        setData(newData);
+        setEditIndex(null);
+    }
+
+    const handleDropdownChange = (event, index) => {
+        console.log("change")
+        const action = event.target.value;
+        console.log(action)
+        if (action === 'Edit') {
+            console.log("what")
+            console.log(index)
+            // const id = data[index].id; // Look up the user's ID based on the index
+            // console.log(id)
+            navigate(`/edit/${data[index].id}, { state: { id: index } }`);
+            setEditIndex(index);
+            // navigate(`/edit/${id}`, { state: { id } });
+            // setEditIndex(id)
+            // HandleEditClick(id);
+        }
+    }
+
+    const HandleEditClick = (id) => {
+        console.log("edit")
+        navigate(`/edit/${id}`, { state: { id } });
+        setEditIndex(id)
+    }
+
     return (
         <div>
             <div style={{ background: "#000000", height: "50px", width: "100%", color: "#ffffff", textAlign: "center", fontSize: "24px" }}>User Management Module</div>
             <div>
+                <Link to="/add"><button style={{ position: "absolute", right: "300px" }} >Add</button></Link>
                 <table>
                     <thead>
                         <tr>
@@ -40,9 +77,9 @@ export default function Home() {
                                     <td>{editIndex === index ? <input type="text" value={user.phone} /> : user.phone}</td>
                                     <td>{user.createdAt}</td>
                                     <td>{user.status ? "Active" : "NOn-Active"}</td>
-                                    <td><select>
-                                        <option>Select an action</option>
-                                        <option onClick={() => handleEditClick(index)}>Edit</option>
+                                    <td><select onChange={(e) => handleDropdownChange(e, index)}>
+                                        <option >Select an action</option>
+                                        <option onClick={() => HandleEditClick(index)}>Edit</option>
                                         <option>Delete</option>
                                     </select></td>
                                 </tr>
@@ -55,6 +92,20 @@ export default function Home() {
                     </tbody>
                 </table>
             </div>
+            {/* <UserTable data={data}/> */}
+            {/* <UserAdd updateUserList={handleAddUser} /> */}
+
+            <Routes>
+                <Route path="/edit/:index" render={({ match }) => (
+                    <UserEdit
+                        user={data[match.params.index]}
+                        // user={data.find(user => user.id === match.params.id)}
+                        handleUpdateUser={handleUpdateUser}
+                        id={match.params.index}
+                    />
+                )} />
+            </Routes>
         </div>
+
     )
 }
