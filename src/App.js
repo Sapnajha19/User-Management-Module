10dfb6746bf87ebdc414612ec5b214ab7a60de79
Sapna Jha <sapnajha1919@gmail.com>
2023-05-12 06:01:16 +0530

    Set up router for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Home from './components/Home';
+import UserEdit from './components/UserEdit';
+import UserAdd from './components/UserAdd';
+
 // import UserAdd from "./components/UserAdd";
 
 function App() {
+  
   return (
-    <div className="App">
-      <Home/>
-      {/* <UserAdd/> */}
-    </div>
+
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/user" element={<Home/>} />
+          <Route path="/edit/:id" element={<UserEdit />} />
+          <Route path="/add" element={<UserAdd />} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
